3e2d95ab5aac916c7e93aaf9b9a7b3fcaa2f871d
KOILADA <satish.koilada@capgemini.com>
2026-06-24 17:33:14 +0530

    modified v1-ec2,../.gitignore

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-08f44e8eca9095668"
     instance_type = "t2.micro"
     key_name = "dpp"
 }
